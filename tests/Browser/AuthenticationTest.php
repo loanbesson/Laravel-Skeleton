053c9eb7ec0d78ceacb2198429b8b8c96ef62f39
Loan Besson <loan.besson@hotmail.fr>
2022-11-11 18:11:56 +0100

    :white_check_mark: Added authentication test and docker fix for Mac

---
+++ tests/Browser/AuthenticationTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class AuthenticationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * Register test.
+     *
+     * @return void
+     */
+    public function testAuthentication()
+    {
+        $user = User::factory()->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                    ->visit('/dashboard')
+                    ->assertSee('Dashboard')
+                    ->logout()
+                    ->assertGuest();
+        });
+    }
+}
